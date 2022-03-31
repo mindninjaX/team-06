72fe6d714c3cae42838ecda8629b55100ba04d53
mandyliao66 <51842785+mandyliao66@users.noreply.github.com>
2022-03-31 17:05:34 +0400

    Update ecg_graph.js

--- ecg_graph.js
+++ ecg_graph.js
@@ -1,53 +1,16 @@
-///template -- will change
 
-import React from "react";
-import App from './App';
+//figure out legnth and time increment
 
-var React = require('react');
-var Component = React.Component;
-var CanvasJSReact = require('./canvasjs.react');
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
- 
-class App extends Component {
-	constructor() {
-		super();
-		this.generateDataPoints = this.generateDataPoints.bind(this);
-	}
-	
-	generateDataPoints(noOfDps) {
-		var xVal = 1, yVal = 100;
-		var dps = [];
-		for(var i = 0; i < noOfDps; i++) {
-			yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
-			dps.push({x: xVal,y: yVal});	
-			xVal++;
-		}
-		return dps;
-	}
-	
-	render() {
-		const options = {
-			theme: "light2", // "light1", "dark1", "dark2"
-			animationEnabled: true,
-			zoomEnabled: true,
-			title: {
-				text: "Try Zooming and Panning"
-			},
-			data: [{
-				type: "area",
-				dataPoints: this.generateDataPoints(500)
-			}]
-		}
-		
-		return (
-		<div>
-			<CanvasJSChart options = {options} 
-				/* onRef={ref => this.chart = ref} */
-			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		</div>
-		);
-	}
+//example array
+var array = [3,3,3,3,3,3,3.5,4,4.3,3,-1,10,3,2,5,6,3,3,3,3,3,3];
+
+var xValues = [];
+
+for (var x = 0; x <= array.length; x += 1) {
+  xValues.push(x);
 }
- 
-module.exports = App;
+
+var data = [{x:xValues, y:array, mode:"scatter"}];
+var layout = {title: "ECG"};
+
+Plotly.newPlot('myDiv', data, layout, {scrollZoom: true});
